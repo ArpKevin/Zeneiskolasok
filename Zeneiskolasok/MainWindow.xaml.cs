71bfcc5d5216ae2e3a0b39c8c1461c689c5619a4
Kevin <arpaskevin11@gmail.com>
2024-10-18 13:16:35 +0200

    Add project files.

---
+++ Zeneiskolasok/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zeneiskolasok
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        internal List<Zenedarab> zenedarabok = new List<Zenedarab>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (var item in File.ReadAllLines(@"..\..\..\src\zene.txt"))
+            {
+                zenedarabok.Add(new(item));
+            }
+            listboxZeneiDarabok.ItemsSource = zenedarabok.Select(z => z.Cim);
+
+        }
+
+        private void Szerzok_Listazas(object sender, RoutedEventArgs e)
+        {
+            listboxZeneszerzok.ItemsSource = zenedarabok.Select(z => z.Szerzo).Distinct().OrderBy(x => x);
+        }
+
+        private void DarabKeletkezese_Listazas(object sender, RoutedEventArgs e)
+        {
+            textblockDarabokKeletkezesiEve.Text = $"A komolyzenei darabok keletkezési éve {zenedarabok.Min(z => z.KeletkezesiEv)} és {zenedarabok.Max(z => z.KeletkezesiEv)} között terjed.";
+            textblockDarabokKeletkezesiEve.Visibility = Visibility.Visible;
+        }
+
+        private void TizesNehezseguDarabokSzama_Listazas(object sender, RoutedEventArgs e)
+        {
+            textblockTizesNehezseguDarabokSzama.Text = $"Az adatfájlban {zenedarabok.Count(z => z.Nehezseg == 10)} db 10-es nehézségű komolyzenei darab van.";
+            textblockTizesNehezseguDarabokSzama.Visibility = Visibility.Visible;
+        }
+
+        private void DebussySzamai_Listazas(object sender, RoutedEventArgs e)
+        {
+            textblockDebussySzamai.Text = $"Az adatfájlban {zenedarabok.Count(z => z.Szerzo.Contains("Debussy"))} db Debussy szerzőjű komolyzenei darab van.";
+            textblockDebussySzamai.Visibility = Visibility.Visible;
+        }
+    }
+}
---
+++ Zeneiskolasok/Zenedarab.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zeneiskolasok
+{
+    internal class Zenedarab
+    {
+        public string Cim { get; set; }
+        public int KeletkezesiEv { get; set; }
+        public int Nehezseg { get; set; }
+        public string Szerzo { get; set; }
+
+        public Zenedarab(string sor)
+        {
+            var x = sor.Split(";");
+            Cim = x[0];
+            KeletkezesiEv = int.Parse(x[1]);
+            Nehezseg = int.Parse(x[2]);
+            Szerzo = x[3];
+        }
+
+        public override string ToString()
+        {
+            return $"Cím: {Cim}, Keletkezési év: {KeletkezesiEv}, Nehézség: {Nehezseg}, Szerző: {Szerzo}";
+        }
+    }
+}
